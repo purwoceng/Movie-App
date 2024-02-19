788ff1919a23cc8dc5d2694fe41b4f3ff045bfa1
Ryan Gonz <wisnu.2722@students.amikom.ac.id>
2024-02-19 11:00:59 +0700

    create prisma schema

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import app from "./app.js";
 
 config();
 
-const port = process.env.PORT || 5000;
+const port = process.env.APP_PORT || 5000;
 app.listen(port, () =>
   console.log(`Server running on http://localhost:${port}`)
 );
